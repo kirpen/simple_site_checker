8af5fb193cbb52a63efb72ae7dcba63843a9dbdf
kirpen <kirpen87@gmail.com>
2020-01-11 02:19:30 +0200

    add: rescue for network errors

--- site_checker.rb
+++ site_checker.rb
@@ -6,13 +6,13 @@ require 'rufus/scheduler'
 require 'daemons'
 
 options = {
-  log_output: true,
-  backtrace:  true,
+  log_output: false,
+  backtrace:  false,
   multiple:   false
 }
 
 POKUPON_URLS = ['https://pokupon.ua', 'https://partner.pokupon.ua'] # ['https://httpstat.us/500', 'https://httpstat.us/400']
-SEND_UP_STATUS = false
+SEND_UP_STATUS = true
 
 Mail.defaults do
   delivery_method :smtp, address: 'localhost', port: 1025
@@ -21,8 +21,8 @@ end
 class MailSender
   def initialize
     @mail = Mail.new do
-      from    'site-status@pokupon.ua'
-      to      'alert@pokupon.ua'
+      from 'site-status@pokupon.ua'
+      to   'alert@pokupon.ua'
     end
   end
 
@@ -32,9 +32,14 @@ class MailSender
     @mail
   end
 
-  def negative(url, status)
+  def negative(url, status=nil, network_error_msg=nil)
     @mail.subject = 'Oops something went wrong'
     @mail.body = "Oops something went wrong with #{url}. Status code: #{status}"
+    
+    unless network_error_msg.nil?
+      @mail.body = "There are network problems with #{url}, domain and network verification is required"
+    end
+    
     @mail
   end
 end
@@ -42,21 +47,32 @@ end
 class StatusChecker
   def initialize(urls)
     @urls = urls
-    @db = Array.new(urls.length, "200")
+    @db = Array.new(urls.length, '200')
   end
 
   def check
     @urls.each_with_index do |url, index|
       previous_status = @db[index]
-      current_status = check_response(url)
-      if previous_status != current_status
-        if current_status == '200' && SEND_UP_STATUS
-          MailSender.new.positive(url, current_status).deliver!
-        else
-          MailSender.new.negative(url, current_status).deliver!
+      
+      begin # if network or domain error
+        current_status = check_response(url)
+        if previous_status != current_status
+          if current_status == '200'
+            MailSender.new.positive(url, current_status).deliver! if SEND_UP_STATUS
+          else
+            MailSender.new.negative(url, current_status).deliver!
+          end
+        end
+        @db[index] = current_status
+
+      rescue => e
+        current_status = 'network_error'
+        if previous_status != current_status
+          MailSender.new.negative(url, current_status, e.message).deliver!
+          @db[index] = current_status
         end
       end
-      @db[index] = current_status
+
     end
   end
 
